e5164a9d7e91178c7f045dd3eeef0e39e8f4faf9
Eñaut Mendiluze Usandizaga <mendienaut@gmail.com>
2024-10-11 13:36:13 +0200

    Add expectation values implementation

--- fileExecution.py
+++ fileExecution.py
@@ -2,10 +2,12 @@ import sys
 import os
 import pickle
 from qiskit import QuantumCircuit
+from qiskit.quantum_info import SparsePauliOp
 from qiskit_aer import AerSimulator
 from qiskit_aer.noise import NoiseModel
 from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import EstimatorV2 as Estimator
 
 # Assuming you already have setup your IBM quantum account
 service = QiskitRuntimeService()
@@ -39,13 +41,35 @@ def output_distribution(qc_init):
     isa_circuit_noisy = pm_noisy.run(qc_init)
 
     # Run and get counts
-    num_shots = 1024
+    num_shots = 10000
     job = ideal_simulator.run(isa_circuit_ideal, shots=num_shots, seed=42)
     ideal_counts = job.result().get_counts(isa_circuit_ideal)
     job = noisy_simulator.run(isa_circuit_noisy, shots=num_shots, seed=42)
     noisy_counts = job.result().get_counts(isa_circuit_noisy)
 
     return ideal_counts, noisy_counts
+
+def expectation_values(qc_init):
+    # Step 2: Define observable
+    observable = SparsePauliOp("Z" * qc_init.num_qubits)
+    # Transpile
+    pm_ideal = generate_preset_pass_manager(backend=ideal_simulator, optimization_level=0, seed_transpiler=128)
+    pm_noisy = generate_preset_pass_manager(backend=noisy_simulator, optimization_level=0, seed_transpiler=128)
+    isa_circuit_ideal = pm_ideal.run(qc_init)
+    isa_circuit_noisy = pm_noisy.run(qc_init)
+
+    ideal_estimator = Estimator(ideal_simulator, options={'seed_simulator': 42})
+    noisy_estimator = Estimator(noisy_simulator, options={'seed_simulator': 42})
+
+    job_ideal = ideal_estimator.run([(isa_circuit_ideal, observable)])
+    result_ideal = job_ideal.result()[0]
+    ideal_expectation = result_ideal.data.evs
+
+    job_noisy = noisy_estimator.run([(isa_circuit_noisy, observable)])
+    result_noisy = job_noisy.result()[0]
+    noisy_expectation = result_noisy.data.evs
+
+    return ideal_expectation, noisy_expectation
 
 
 def final_density_matrix(qc_init):
@@ -61,7 +85,7 @@ def final_density_matrix(qc_init):
     isa_circuit_noisy = pm_noisy.run(qc_init)
 
     # Run and get density matrix
-    num_shots = 1024
+    num_shots = 10000
     job = ideal_simulator.run(isa_circuit_ideal, shots=num_shots, seed=42)
     ideal_dm = job.result().data().get('density_matrix')
     job = noisy_simulator.run(isa_circuit_noisy, shots=num_shots, seed=42)
@@ -119,6 +143,7 @@ def process_file(filepath, base_input_dir, base_output_dir):
             qc_init_ps = ps_input.copy()
             qc_init_ps = qc_init_ps.compose(qc)
             ps_ideal_out_dist, ps_noisy_out_dist = output_distribution(qc_init_ps)
+            ps_ideal_exp_val, ps_noisy_exp_val = expectation_values(qc_init_ps)
             ps_ideal_dm, ps_noisy_dm = final_density_matrix(qc_init_ps)
 
             # Prepare PureState data row
@@ -126,8 +151,10 @@ def process_file(filepath, base_input_dir, base_output_dir):
                 'Name': filepath,
                 'Input': f'PureState_{index}',
                 'Ideal_output_distribution': ps_ideal_out_dist,
+                'Ideal_expectation_value': ps_ideal_exp_val,
                 'Ideal_density_matrix': ps_ideal_dm,
                 'Noisy_output_distribution': ps_noisy_out_dist,
+                'Noisy_expectation_value': ps_noisy_exp_val,
                 'Noisy_density_matrix': ps_noisy_dm
             }
             results.append(pure_state_row)
@@ -135,6 +162,7 @@ def process_file(filepath, base_input_dir, base_output_dir):
             qc_init_qt = qt_input.copy()
             qc_init_qt = qc_init_qt.compose(qc)
             qt_ideal_out_dist, qt_noisy_out_dist = output_distribution(qc_init_qt)
+            qt_ideal_exp_val, qt_noisy_exp_val = expectation_values(qc_init_qt)
             qt_ideal_dm, qt_noisy_dm = final_density_matrix(qc_init_qt)
 
             # Prepare Quratest data row
@@ -142,8 +170,10 @@ def process_file(filepath, base_input_dir, base_output_dir):
                 'Name': filepath,
                 'Input': f'Quratest_{index}',
                 'Ideal_output_distribution': qt_ideal_out_dist,
+                'Ideal_expectation_value': qt_ideal_exp_val,
                 'Ideal_density_matrix': qt_ideal_dm,
                 'Noisy_output_distribution': qt_noisy_out_dist,
+                'Noisy_expectation_value': qt_noisy_exp_val,
                 'Noisy_density_matrix': qt_noisy_dm
             }
             results.append(quratest_row)
